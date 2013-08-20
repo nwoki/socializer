f31414e57656e6dbb592ea00811019d2c279c2c4
Francesco Nwokeka <francesco.nwokeka@gmail.com>
2013-08-20 13:02:16 +0200

    cleaned up examples mains

--- examples/facebook-login/main.cpp
+++ examples/facebook-login/main.cpp
@@ -19,52 +19,8 @@ int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
 
-//     // GoWar info
-//     Socializer::Foursquare *foursquare = new Socializer::Foursquare("UINBCI54PE04OECTALA03CFOO5YEGIOL5Q1EZ24CTXKJX4Z3"
-//                                                                     , "http://www.playgowar.com");
-//     qDebug() << foursquare->obtainAuthPageUrl();
-
-
-    // QML component test
-//     QApplication app(argc, argv);
-//     QDeclarativeView *view = new QDeclarativeView;
-
-    // FACEBOOK
-//     Socializer::Facebook *fb = new Socializer::Facebook("253316734751940"
-//                                                       , "http://www.2s2h.com");
-//     fb->enableScopeUserInfo(true);           // TODO test this after setting context property
-// //     fb->enableScopePublishActions(true);        // TODO test this after setting context property
-//     fb->setContextProperty(view);
-//     view->setSource(QUrl("qmlcomponents/FacebookQML.qml"));
-
-    // with auth token
     Socializer::Facebook *fb = new Socializer::Facebook("CAACEdEose0cBAIW5kPJCF2r2hWFTOKM91vGqCFzf2IMKg7nCC4zx6OPVQRZCoKqq8IUqkwsx1qlaOGm3TrCaZB1FxoAKCWZAdMgNgniuQslLKdxtZA9pmlSk61gv3tGcZBh2NJdZCAvw8Weh4FZA3yqZC4s6eRQAXAkZD");
     fb->enableScopeUserInfo(true);
-
-    // TWITTER
-//     Socializer::Twitter *twitter = new Socializer::Twitter("ihWJWsvkIJrTNmMAssh1ZA"
-//                                                          , "http://www.2s2h.com"
-//                                                          , "MuQN5yLBWjG5D7CWJ0YCL5O7AfOOhWsDJyq1iSGfb0");
-//     twitter->obtainAuthPageUrl();       // TODO test this after setting context property
-//     twitter->setContextProperty(view);
-//     view->setSource(QUrl("qm/*l*/components/TwitterQML.qml"));
-
-    // LINKEDIN
-//     Socializer::LinkedIn *link = new Socializer::LinkedIn("1fx5y8590q6r", "1tTwdCxMuiycme2R", "http://www.2s2h.com");
-// 
-//     link->setFullProfileScope(true);
-//     link->setEmailProfileScope(true);
-// 
-//     link->obtainAuthPageUrl();          // TODO test this after setting context property
-//     link->setContextProperty(view);
-//     view->setSource(QUrl("/home/nwoki/GitProjects/Ispirata/R&D/socializer/qmlcomponents/LinkedinQML.qml"));
-
-
-
-//     view->setFixedSize(480, 600);
-//     view->show();
-
-
 
     return app.exec();
 }
--- examples/linkedin-login/main.cpp
+++ examples/linkedin-login/main.cpp
@@ -15,20 +15,14 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
-//     Socializer::LinkedIn *link = new Socializer::LinkedIn("4b05dbf3-36cf-4539-8936-0754854f6303");
-    qDebug() << "aaaaaaaa - " << argv[1];
 
-//     Socializer::LinkedIn *link = new Socializer::LinkedIn(argv[1], "1fx5y8590q6r", "1tTwdCxMuiycme2R", "http://www.2s2h.com");
-    Socializer::LinkedIn *link = new Socializer::LinkedIn("AQUkEMYehs5HFfh-3A6x9s6vEgD_Oh9I6_DIYm_yClbFF9cbgpLk7jDkRwMa-jwjRgfN3UO_9suywnwF_-yEMgET3MtB8z6pExBf5gFAQunD_rdxIAA9YkQqquVq5XjbPxzZR5ckS3SQd2-FDH0-C7SCIKNObzUKnbifqqpPGZM1juZvhcY");
-/*
- * AQUKV6dAQGnBIcP3ZRwo1ZKpVdsfKD-3UzC9UL6Z3T4h_eFHVfeaFZOpdDX7T3oI3bPLzPbj6_L276NpEpKo6QGUHNg9kcv6cUS2lwt__3ciGo-LSyb2BI3celhEMKnV2u80kry7EwdctF8yDxxjP6avi8Sx6r8xBfZHf3OnwcMC0OWJplA
- */
-//     link->setFullProfileScope(true);
-//     link->setEmailProfileScope(true);
+    if (argc == 2) {
+        // use auth CODE
+        Socializer::LinkedIn *link = new Socializer::LinkedIn(argv[1], "1fx5y8590q6r", "1tTwdCxMuiycme2R", "http://www.2s2h.com");
+    } else {
+        Socializer::LinkedIn *link = new Socializer::LinkedIn("AQUkEMYehs5HFfh-3A6x9s6vEgD_Oh9I6_DIYm_yClbFF9cbgpLk7jDkRwMa-jwjRgfN3UO_9suywnwF_-yEMgET3MtB8z6pExBf5gFAQunD_rdxIAA9YkQqquVq5XjbPxzZR5ckS3SQd2-FDH0-C7SCIKNObzUKnbifqqpPGZM1juZvhcY");
+    }
 
-//     link->obtainAuthPageUrl();          // TODO test this after setting context property
-
-// link->requestAuthToken("AQR-K9faN0XAcUoe-s57WR8YmpMNrHG2MRrokmCI0W9THERgvJ0aIXyQRqnHriJDW7pmhhq7ANPQuRAA59Hs6bjLYlNepK7u6c85zfbTNZFYMTzYLEI");
 
     return app.exec();
 }
