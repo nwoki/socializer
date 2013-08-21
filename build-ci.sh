a61aa0a61a5120985336d356d000aaa854904176
Francesco Nwokeka <francesco.nwokeka@ispirata.com>
2013-08-21 12:07:14 +0200

    added qt4 build

--- build-ci.sh
+++ build-ci.sh
@@ -22,8 +22,14 @@ cd $DEPS_DIR
 
 cd $MAIN_DIR
 
-# Build
-mkdir build
-cd build
+# Build qt5
+mkdir buildQt5
+cd buildQt5
 cmake .. -DQt5_DIR=/opt/Qt5.1.0/5.1.0/gcc_64/lib/cmake/Qt5 -DCommonModules_DIR=$MAIN_DIR/deps/install/lib/cmake/CommonModules || return 1
+make -j3 || return 1
+
+# Build qt4
+mkdir buildQt4
+cd buildQt4
+cmake .. -DCommonModules_DIR=$MAIN_DIR/deps/install/lib/cmake/CommonModules || return 1
 make -j3 || return 1
