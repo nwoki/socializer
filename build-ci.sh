b134e50daa4c5761e1d0260ddd84152964520c32
Francesco Nwokeka <francesco.nwokeka@ispirata.com>
2013-08-21 12:12:46 +0200

    fixed qt5 build script

--- build-ci.sh
+++ build-ci.sh
@@ -25,8 +25,11 @@ cd $MAIN_DIR
 # Build qt5
 mkdir buildQt5
 cd buildQt5
-cmake .. -DQt5_DIR=/opt/Qt5.1.0/5.1.0/gcc_64/lib/cmake/Qt5 -DCommonModules_DIR=$MAIN_DIR/deps/install/lib/cmake/CommonModules || return 1
+cmake .. -DQT5_BUILD=TRUE -DQt5_DIR=/opt/Qt5.1.0/5.1.0/gcc_64/lib/cmake/Qt5 -DCommonModules_DIR=$MAIN_DIR/deps/install/lib/cmake/CommonModules || return 1
 make -j3 || return 1
+
+# back to main dir
+cd $MAIN_DIR
 
 # Build qt4
 mkdir buildQt4
