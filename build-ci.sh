ffcb1df919faf9e62fea7d7f0e4e40583280823b
Francesco Nwokeka <francesco.nwokeka@gmail.com>
2015-09-10 10:24:02 +0200

    removed uneeded cmake common modules
    Signed-off-by: Francesco Nwokeka <francesco.nwokeka@gmail.com>

--- build-ci.sh
+++ build-ci.sh
@@ -2,18 +2,12 @@
 export MAIN_DIR=$(pwd)
 
 # Cleanup previous builds
-rm -rf deps
 rm -rf build*
-
-# Dependencies
-mkdir deps
-cd deps
-export DEPS_DIR=$(pwd)
 
 # Build qt5
 mkdir buildQt5
 cd buildQt5
-cmake .. -DQT5_BUILD=TRUE -DQt5_DIR=/opt/Qt5.1.0/5.1.0/gcc_64/lib/cmake/Qt5 -DCommonModules_DIR=$MAIN_DIR/deps/install/lib/cmake/CommonModules || return 1
+cmake .. -DQT5_BUILD=TRUE -DQt5_DIR=/opt/Qt5.1.0/5.1.0/gcc_64/lib/cmake/Qt5 || return 1
 make -j3 || return 1
 
 # back to main dir
@@ -22,5 +16,5 @@ cd $MAIN_DIR
 # Build qt4
 mkdir buildQt4
 cd buildQt4
-cmake .. -DCommonModules_DIR=$MAIN_DIR/deps/install/lib/cmake/CommonModules || return 1
+cmake ..  || return 1
 make -j3 || return 1
